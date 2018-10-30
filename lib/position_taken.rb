b3ee777fad52e2b5a16a7e923d9db5c336c093a2
MC Simoes <mariacristina.simoes@gmail.com>
2018-10-30 17:38:06 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -22,9 +22,10 @@ end
 # code your #position_taken? method here!
 
 def #position_taken?(board, index)
-if board[index] == " " || " "
-  return FALSE
-elsif return TRUE
+if board[index] == " " || " " || ""
+  return false
+elsif board[index] == "X" || "O"
+return true
 end
 
 
