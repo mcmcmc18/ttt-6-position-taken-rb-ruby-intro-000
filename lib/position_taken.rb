94465c3fa873b171569f1879bf9db4d758a86589
MC Simoes <mariacristina.simoes@gmail.com>
2018-10-30 17:57:40 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,7 +4,7 @@ def position_taken?(board, index)
   taken = nil
 if (board[index] == " " ||  board[index] == "" || board[index] == nil)
   taken = false
-else board[index] == "X" || board[index] == "O"
+else
 taken = true
 end
 
