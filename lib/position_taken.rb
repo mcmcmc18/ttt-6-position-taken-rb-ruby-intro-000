b86123a04740973e7ad8365fd96c4e834c11c4d3
MC Simoes <mariacristina.simoes@gmail.com>
2018-10-30 18:03:11 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,4 @@
-# code your #position_taken? method here!
-ttt_board = ["",""," "," "," "," "," "," ",""]
-
+# code your #position_taken? method here
 def position_taken?(board, index)
 taken = nil
 if (board[index] ==  " " || board[index] == "" || board[index] == nil)
